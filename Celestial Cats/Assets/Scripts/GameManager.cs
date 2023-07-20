fd22109e75a112baa2d95b923fe32c4cc3a11a04
Ashley Roesler <ashley.n.roesler@gmail.com>
2023-07-20 16:59:32 -0500

    added text mesh pro essentials, basic main menu functionality

---
+++ Celestial Cats/Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public string MainMenuSceneName;
+    public string GameSceneName;
+   // public string EndSceneName;
+
+    [Space]
+    public GameObject LoadingScreenCanvas;
+    public Image LoadingScreen;
+
+    [Space]
+    public float LoadTime = 1f;
+
+    public void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayGame() {
+        StartCoroutine(LoadScene_Coroutine(GameSceneName));
+    }
+
+    public void GoToMainMenu() {
+        StartCoroutine(LoadScene_Coroutine(MainMenuSceneName));
+    }
+
+   /* public void EndGame() {
+        StartCoroutine(LoadScene_Coroutine(EndSceneName));
+    }*/
+
+    private IEnumerator LoadScene_Coroutine(string scene) {
+
+        LoadingScreenCanvas.SetActive(true);
+
+        Color c = LoadingScreen.color;
+
+        // fade in loading screen
+        for (float alpha = 0f; alpha <= 1f; alpha += 0.2f) {
+            c.a = alpha;
+            LoadingScreen.color = c;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        // load scene
+        SceneManager.LoadScene(scene);
+
+        yield return new WaitForSeconds(LoadTime);
+
+        // fade out loading screen
+        for (float alpha = 1f; alpha >= 0f; alpha -= 0.2f) {
+            c.a = alpha;
+            LoadingScreen.color = c;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        LoadingScreenCanvas.SetActive(false);
+    }
+
+    public void QuitGame() {
+
+        // to do: make sure to save which pages the player has seen
+
+#if UNITY_EDITOR
+        Debug.Log("Game quit!");
+#endif
+
+        Application.Quit();
+    }
+}
