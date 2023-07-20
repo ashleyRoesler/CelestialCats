778275522832e0c160ec710daec343a4f1cd77d5
Ashley Roesler <ashley.n.roesler@gmail.com>
2023-07-20 17:19:00 -0500

    scrolling level background

---
+++ Celestial Cats/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+// tutorial: https://youtu.be/-6H-uYh80vc
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public RawImage Background;
+
+    public float XSpeed;
+    public float YSpeed;
+
+    void Update()
+    {
+        Background.uvRect = new Rect(Background.uvRect.position + new Vector2(XSpeed, YSpeed) * Time.deltaTime, Background.uvRect.size);
+    }
+}
