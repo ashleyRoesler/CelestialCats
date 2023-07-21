14a8a633172729ce6f1ab55470b38fb52e981212
Ashley Roesler <ashley.n.roesler@gmail.com>
2023-07-20 20:59:26 -0500

    player movement

--- Celestial Cats/Assets/Scripts/PlayerController.cs
+++ Celestial Cats/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,34 @@
 using UnityEngine;
 
+// tutorial: https://youtu.be/u8tot-X_RBI
+
 public class PlayerController : MonoBehaviour {
 
-    private void Update() {
+    public float MovementSpeed = 1.0f;
 
+    public Rigidbody2D Rigidbody;
+
+    private Vector2 _moveDirection;
+
+    // note to self: Update depends on framerate, good for processing input
+    private void Update() {
+        HandleInput();
+    }
+
+    // note to self: FixedUpdate happens a set amount of times per frame, good for physics calculations
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void HandleInput() {
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        _moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void Move() {
+        Rigidbody.velocity = new Vector2(_moveDirection.x * MovementSpeed, _moveDirection.y * MovementSpeed);
     }
 }
